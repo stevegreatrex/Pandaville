3b219d735a73f4e711317c1939a0497ff5fe0a3a
Steve Greatrex <stephen.greatrex@googlemail.com>
2013-01-15 20:23:44 +0000

    Added creation of blank model

--- src/gameworld.js
+++ src/gameworld.js
@@ -1,6 +1,15 @@
 ﻿define(function () {
     var GameWorld = function () {
+        var self = this,
+            model = {
+            money: 0,
+            xp: 0,
+            buildings: []
+        };
 
+        self.getModel = function () {
+            return model;
+        }
     };
 
     return GameWorld;
