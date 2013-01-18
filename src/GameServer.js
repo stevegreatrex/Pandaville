d6499d24cea0a294507efc0552020c0ebbdb4a86
Steve Greatrex <stephen.greatrex@googlemail.com>
2013-01-18 12:27:01 +0000

    Refactoring addBuilding
    Refactored to allow any gameworld method to (in theory) be executed
    against the server

--- src/GameServer.js
+++ src/GameServer.js
@@ -1,34 +1,40 @@
 ﻿define(["underscore", "jquery", "GameWorld", "dataSource", "Errors"], function (_, $, GameWorld, dataSource, Errors) {
     var GameServer = function () {
 
-    };
+    },
+    wrapWorldAction = function (action) {
+        return function () {
+            var deferred   = $.Deferred(),
+                id         = arguments[0],
+                actionArgs = Array.prototype.slice.call(arguments, 1),
 
-    GameServer.prototype.addBuilding = function (id, building) {
-        var deferred = $.Deferred(),
-        //handle successful retrieval of the model from the data source
-        onModelRetrieved = function (model) {
-            var world = new GameWorld(model);
+            //handle successful retrieval of the model from the data source
+            onModelRetrieved = function (model) {
+                var world = new GameWorld(model);
 
-            if (!world.addBuilding.canExecute(building)) {
-                deferred.reject(Errors.invalidAction, model);
-            } else {
-                world.addBuilding(building);
-                var updatedModel = world.getModel();
-                dataSource.updateModel(id, updatedModel)
-                    .done(_.bind(deferred.resolve, null, updatedModel))
-                    .fail(function(error) {
-                        deferred.reject(error, model);
-                    });
-            }
+                if (!world[action].canExecute.apply(this, actionArgs)) {
+                    deferred.reject(Errors.invalidAction, model);
+                } else {
+                    world[action].apply(this, actionArgs);
+                    var updatedModel = world.getModel();
+                    dataSource.updateModel(id, updatedModel)
+                        .done(_.bind(deferred.resolve, this, updatedModel))
+                        .fail(function(error) {
+                            deferred.reject(error, model);
+                        });
+                }
+            };
+
+            //get the model from the datasource
+            dataSource.getModel(id)
+                .done(onModelRetrieved)
+                .fail(deferred.reject);
+
+            return deferred.promise();
         };
-        dataSource.getModel(id)
-            .done(onModelRetrieved)
-            .fail(function(error) {
-                deferred.reject(error);
-            });
-
-        return deferred.promise();
     };
+
+    GameServer.prototype.addBuilding = wrapWorldAction("addBuilding");
 
     return GameServer;
 });
