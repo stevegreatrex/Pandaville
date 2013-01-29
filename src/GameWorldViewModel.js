02cbc634dcfe437fdf3d163be571193ff95ac70a
Steve Greatrex <stephen.greatrex@googlemail.com>
2013-01-29 20:07:10 +0000

    Data-bound SVG elements
    Making a start on using ko databinding to render gameworld

--- src/GameWorldViewModel.js
+++ src/GameWorldViewModel.js
@@ -1,37 +1,7 @@
-﻿define(["jquery", "knockout", "knockout.mapping", "GameWorld", "command"], function ($, ko, mapping, GameWorld) {
+﻿define(["jquery", "knockout", "knockout.mapping", "GameWorld", "command", "knockout.bindingHandlers"], function ($, ko, mapping, GameWorld) {
     var ViewModel = function (id, api, initialModel) {
         var self      = mapping.fromJS(initialModel),
             gameWorld = new GameWorld(initialModel),
-            createEmptyArray = function (size, insertArray) {
-                var result = [];
-                for (var i = 0; i < size; i++) {
-                    result.push(insertArray ? createEmptyArray(self.size.x()) : null);
-                }
-                return result;
-            },
-            refreshGrid = function () {
-                var buildings = self.buildings(),
-                    rows = createEmptyArray(self.size.y(), true);
-
-                for (var i = 0; i < buildings.length; i++) {
-                    var building = buildings[i],
-                        buildingPos = building.position;
-
-                    rows[buildingPos.y()][buildingPos.x()] = building;
-                    clearBuildingSpace(rows, building);
-                }
-
-                self.grid(rows);
-            },
-            clearBuildingSpace = function (grid, building) {
-                for (var y = 0; y < building.size.y(); y++) {
-                    for (var x = 0; x < building.size.x() ; x++) {
-                        if (x + y > 0) { //skip [0,0]
-                            grid[building.position.y() + y].splice([building.position.x() + x], 1);
-                        }
-                    }
-                }
-            },
             refreshModelOnError = function (err, model) {
                 gameWorld.setModel(model);
                 updateObservableProperties();
@@ -52,9 +22,6 @@
             return api.addBuilding(building);
         }).done(gameWorld.setModel).fail(refreshModelOnError);
 
-        self.buildings.subscribe(refreshGrid);
-
-        refreshGrid();
         return self;
     };
 
--- src/knockout.bindingHandlers.js
+++ src/knockout.bindingHandlers.js
@@ -1,5 +1,24 @@
-﻿define(["knockout"], function (ko) {
-	ko.bindingHandlers.for = {
+﻿define(["jquery", "knockout"], function ($, ko) {
+    ko.bindingHandlers.worldSize = {
+        update: function (element, valueAccessor) {
+            var $element = $(element),
+                value = ko.utils.unwrapObservable(valueAccessor());
 
+            var scaleX = $element.width() / ko.utils.unwrapObservable(value.x),
+                scaleY = $element.height() / ko.utils.unwrapObservable(value.y);
+
+            $element.data("scale", Math.max(scaleX, scaleY));
+        }
+    };
+	ko.bindingHandlers.scaleToWorld = {
+		update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+			var $svg =  $(element).parents("svg");
+
+			ko.bindingHandlers.attr.update.call(this, element, function () {
+			    return {
+                    transform: "scale(" + $svg.data("scale") + ")"
+			    }
+			}, allBindingsAccessor, viewModel);
+		}
 	};
 });
